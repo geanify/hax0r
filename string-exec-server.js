f9d56f77334ceb71bfd9987e30912dd2f03c707b
geanify <geanify@gmail.com>
2024-03-29 14:23:34 +0200

    Basic XSS

---
+++ string-exec-server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const bodyParser = require("body-parser");
+const app = express();
+
+
+let penis = '';
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send(penis)
+})
+
+app.post("/str-exec", (req,res)=> {
+  console.log(req.body)
+  penis = req.body.test;
+  res.send(req.body);
+})
+
+
+app.listen(8080);
+/*
+var body = {
+test: '<script>alert("haxor")</script>'
+}
+
+fetch("/str-exec", {
+  method: "POST",
+ headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+	body: JSON.stringify(body),
+})
+
+*/
